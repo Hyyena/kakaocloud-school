a35b9d69eaccb5c8c68018e00af23d62aa9c25b5
Hyyena <sjtxm0320@gmail.com>
2022-11-25 18:23:41 +0900

    ✨ TIL: 221125

---
+++ FE/Node-project/221125/App.js
@@ -0,0 +1,369 @@
+const express = require("express");
+const morgan = require("morgan");
+const compression = require("compression");
+const path = require("path");
+const mysql = require("mysql");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const multer = require("multer");
+const dotenv = require("dotenv");
+
+// 설정 파일의 내용 가져오기
+dotenv.config();
+
+// 서버 설정
+const app = express();
+app.set("port", process.env.PORT || 9000);
+
+// 로그를 매일 기록하기 위한 설정
+let FileStreamRotator = require("file-stream-rotator");
+let fs = require("fs");
+
+// 로그를 기록할 디렉토리 경로 생성
+let logDirectory = path.join(__dirname, "log");
+
+// 디렉토리가 없으면 생성
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+// 로그 파일 옵션을 설정
+let accessLogStream = FileStreamRotator.getStream({
+  date_format: "YYYYMMDD",
+  filename: path.join(logDirectory, "access-%DATE%.log"),
+  frequency: "daliy",
+  verbose: false,
+});
+
+// 로그 기록 설정
+app.use(morgan("combined", { stream: accessLogStream }));
+
+// 압축해서 전송하는 옵션 설정
+app.use(compression());
+
+// POST 방식의 파라미터 읽을 수 있도록 설정
+let bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// 세션을 데이터베이스에 저장하는 작업
+// 데이터베이스 접속 정보
+let options = {
+  host: process.env.HOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+};
+
+// 세션을 저장하기 위한 MySQL 데이터베이스 저장소 생성
+const MariaDBStore = require("express-mysql-session")(session);
+
+// 세션 설정
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new MariaDBStore(options),
+  })
+);
+
+// 파일 업로드 설정
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "public/img");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 10 * 1024 * 1024 },
+});
+
+// 정적 파일의 경로를 설정
+app.use("/", express.static("public"));
+
+// 파일 다운로드를 위한 모듈
+let util = require("util");
+let mime = require("mime");
+
+// 데이터베이스 연결
+let connection = mysql.createConnection(options);
+connection.connect((error) => {
+  if (error) {
+    console.log(error);
+    throw error;
+  }
+});
+
+// 기본 요청을 처리
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// 데이터 전체 가져오기 처리
+app.get("/item/all", (req, res) => {
+  // 템플릿 엔진: res.render(파일 경로, 데이터)
+  // 템플릿 엔진에 넘겨주는 데이터는 프로그래밍 언어의 데이터
+
+  // JSON 출력: res.json(데이터)
+  // json 문자열의 형태로 데이터를 제공
+  // Front End 에서 데이터를 수신해서 출력
+
+  // 2개 이상의 데이터를 조회할 때는 정렬은 필수
+  connection.query(
+    "select * from goods order by itemid desc",
+    (err, results, fields) => {
+      if (err) {
+        // 에러가 발생한 경우
+        // 에러가 발생했다고 데이터를 전송하지 않으면 안됨
+        res.json({ result: false });
+      } else {
+        // 정상 응답을 한 경우
+        res.json({ result: true, list: results });
+      }
+    }
+  );
+});
+
+// 데이터 일부분 가져오기
+// URL은 /item/list
+// 파라미터는 pageno 1개 인데 없으면 1로 설정
+app.get("/item/list", (req, res) => {
+  // 파라미터 읽어오기
+  let pageno = req.query.pageno;
+  if (pageno == undefined) {
+    pageno = 1;
+  }
+  console.log(pageno);
+  // 브라우저에서 테스트 - 콘솔 확인
+  // localhost:9000/item/list
+  // localhost:9000/item/list?pageno=3
+
+  // item 테이블에서 itemid 를 가지고 내림차순 정렬해서
+  // 페이지 단위로 데이터 가져오기
+  // select * from item order by itemid desc limit 시작번호, 5
+  // 시작번호=(pageno-1)*5
+
+  // 성공과 실패 여부 저장
+  let result = true;
+
+  // 성공했을 때 데이터 저장
+  let list;
+
+  // 파라미터는 무조건 문자열입니다.
+  // 파라미터를 가지고 산술연산을 할 때는 숫자로 변환을 수행
+  connection.query(
+    "select * from goods order by itemid desc limit ?, 5",
+    [(parseInt(pageno) - 1) * 5],
+    (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        // res.json({ result: false });
+
+        result = false;
+      } else {
+        // res.json({ result: true, list: results });
+
+        list = results;
+      }
+
+      // goods 테이블의 전체 데이터 개수 가져오기
+      let cnt = 0;
+      connection.query(
+        "select count(*) cnt from goods",
+        [],
+        (err, results, fields) => {
+          if (err) {
+            // 에러가 발생했을 때
+            console.log(err);
+            results = false;
+          } else {
+            // 정상적으로 구문이 실행되었을 때
+            // 하나의 행만 리턴되므로 0번째 데이터를 읽어내면 된다.
+            cnt = results[0].cnt;
+            console.log(cnt);
+          }
+
+          // 응답 생성해서 전송
+          if (results === false) {
+            res.json({ result: false });
+          } else {
+            res.json({ result: true, list: list, count: cnt });
+          }
+        }
+      );
+    }
+  );
+});
+
+// 상세보기 처리를 위한 코드
+app.get("/item/detail/:itemid", (req, res) => {
+  // 파라미터 읽기
+  let itemId = req.params.itemid;
+
+  // itemId를 이용해서 한 개의 데이터를 찾아오는 SQL 실행
+  connection.query(
+    "select * from goods where itemid=?",
+    [itemId],
+    (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        res.json({ result: false });
+      } else {
+        console.log(results);
+        res.json({ result: true, item: results[0] });
+      }
+    }
+  );
+});
+
+// 이미지 다운로드 처리
+app.get("/img/:pictureurl", (req, res) => {
+  let pictureurl = req.params.pictureurl;
+
+  // 이미지 파일의 절대경로 생성
+  let file = `C:/Users/user/Desktop/kakaocloud-school/FE/Node-project/221125/public/img/${pictureurl}`;
+  console.log(__dirname);
+
+  // 파일 이름을 가지고 타입 생성
+  let mimeType = mime.lookup(pictureurl);
+
+  res.setHeader("Content-disposition", `attachment; filename=${pictureurl}`);
+  res.setHeader("Content-type", mimeType);
+
+  // 파일의 내용을 읽어서 res에 전송
+  let fileStream = fs.createReadStream(file);
+  fileStream.pipe(res);
+});
+
+// 현재 날짜를 문자열로 리턴하는 함수
+// 요즘 등장하는 JavaScript 라이브러리들의 샘플 예쩨는
+// 특별한 경우가 아니면 function을 사용하지 않는다.
+const getDate = () => {
+  let date = new Date();
+
+  let year = date.getFullYear();
+
+  // 월은 +1을 해야 우리가 사용하는 월이 된다.
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+
+  month = month >= 10 ? month : "0" + month;
+  day = day >= 10 ? day : "0" + day;
+
+  return `${year}-${month}-${day}`;
+};
+
+// 날짜와 시간을 리턴하는 함수
+const getTime = () => {
+  let date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+
+  hour >= 10 ? hour : "0" + hour;
+  minute >= 10 ? minute : "0" + minute;
+  second >= 10 ? second : "0" + second;
+
+  return `${getDate()} ${hour}:${minute}:${second}`;
+};
+
+// 데이터 삽입을 처리해주는 함수
+app.post("/item/insert", upload.single("pictureurl"), (req, res) => {
+  // 파라미터 읽어오기
+  const itemName = req.body.itemname;
+  const description = req.body.description;
+  const price = req.body.price;
+
+  // 파일 이름
+  // 업로드하는 파일이 없으면 default.png
+  let pictureUrl;
+  if (req.file) {
+    pictureUrl = req.file.filename;
+  } else {
+    pictureUrl = "default.jpg";
+  }
+
+  // 가장 큰 itemid 찾기
+  connection.query(
+    "select max(itemid) maxid from goods",
+    [],
+    (err, results, fields) => {
+      let itemId;
+      // 최대값이 있으면 +1하고, 없으면 1로 설정
+      if (results.length > 0) {
+        itemId = results[0].maxid + 1;
+      } else {
+        itemId = 1;
+      }
+
+      connection.query(
+        "insert into goods(itemid, itemname, price, description, pictureurl, updatedate) values(?, ?, ?, ?, ?, ?)",
+        [itemId, itemName, price, description, pictureUrl, getDate()],
+        (err, results, fields) => {
+          if (err) {
+            console.log(err);
+            res.json({ result: false });
+          } else {
+            // 현재 날짜 및 시간을 update.txt에 기록
+            const writeStream = fs.createWriteStream("./update.txt");
+            writeStream.write(getTime());
+            writeStream.end();
+
+            res.json({ result: true });
+          }
+        }
+      );
+    }
+  );
+});
+
+// 데이터를 삭제하는 함수
+app.post("/item/delete", (req, res) => {
+  // POST 방식으로 전송된 데이터 읽기
+  let itemId = req.body.itemid;
+
+  // itemid를 받아서 goods 테이블에서 삭제하기
+  connection.query(
+    "delete from goods where itemid=?",
+    [itemId],
+    (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        res.json({ result: false });
+      } else {
+        // 현재 날짜 및 시간을 update.txt에 기록
+        const writeStream = fs.createWriteStream("./update.txt");
+        writeStream.write(getTime());
+        writeStream.end();
+
+        res.json({ result: true });
+      }
+    }
+  );
+});
+
+// 수정을 GET으로 요청했을 때, 수정화면으로 이동
+app.get("/item/update", (req, res) => {
+  // public 디렉토리의 update.html을 읽어내서 리턴
+  fs.readFile("./public/update.html", (err, data) => {
+    res.end(data);
+  });
+});
+
+// 에러 발생시 처리
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send(err.message);
+});
+
+// 서버 구동
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
+});
---
+++ FE/Node-project/221125/public/index.js
@@ -0,0 +1,328 @@
+// DOM 찾아오기
+let allbtn = document.getElementById("allbtn");
+
+// 데이터 출력을 위한 영역
+let content = document.getElementById("content");
+
+// allbtn 클릭 이벤트 처리
+allbtn.addEventListener("click", (e) => {
+  // ajax로 데이터 가져오기
+  let request = new XMLHttpRequest();
+
+  // 요청 생성
+  request.open("GET", "/item/all");
+
+  // 요청
+  request.send("");
+
+  // 데이터가 전송된 경우 처리
+  request.addEventListener("load", () => {
+    // 출력을 하기 위해서는 JSON 문자열을 자바스크립트 객체로 변환
+    let data = JSON.parse(request.responseText);
+
+    // 데이터 가져오기에 성공한 경우
+    if (data.result == true) {
+      content.innerHTML = "<div align='center' class='body'>";
+      content.innerHTML += "<h2>상품 목록</h2>";
+      content.innerHTML += "<table border='1'>";
+      content.innerHTML += "<tr class='header'>";
+      content.innerHTML += "<th align='center' width='80'>ID</th>";
+      content.innerHTML += "<th align='center' width='320'>이름</th>";
+      content.innerHTML += "<th align='center' width='100'>가격</th>";
+      content.innerHTML += "</tr>";
+
+      // list 키에 있는 데이터를 가져오기
+      let arr = data.list;
+
+      // 배열의 데이터 순회
+      for (let item of arr) {
+        content.innerHTML += "<tr class='record'>";
+
+        content.innerHTML += "<td align='center'>" + item.itemid + "</td>";
+        content.innerHTML += "<td align='left'>" + item.itemname + "</td>";
+        content.innerHTML += "<td align='right'>" + item.price + "원</td>";
+
+        content.innerHTML += "</tr>";
+      }
+
+      content.innerHTML += "</table>";
+      content.innerHTML += "</div>";
+    } else {
+      content.innerHTML = "데이터 가져오기 실패";
+    }
+  });
+});
+
+let listBtn = document.getElementById("listBtn");
+
+// 현재 페이지 번호를 저장할 변수 선언
+let pageno = 1;
+
+listBtn.addEventListener("click", (e) => {
+  let request = new XMLHttpRequest();
+  request.open("GET", `/item/list?pageno=${pageno}`);
+  request.send("");
+
+  request.addEventListener("load", () => {
+    // 출력 영역을 초기화
+    content.innerHTML = "";
+
+    // 데이터 파싱
+    let data = JSON.parse(request.responseText);
+    console.log(data);
+
+    if (data.result == true) {
+      // 데이터 개수와 목록 가져오기
+      let count = data.count;
+      console.log(count);
+      let list = data.list;
+
+      // 출력 내용 만들기
+      let display = "<div align='center' class='body' >";
+      display += "<h2>상품 목록</h2>";
+      display += "<table border='1' id='tableData'";
+      display += "<tr><td colspan='3' align='right' >";
+      display += `전체 데이터 개수: ${count}개</td></tr>`;
+
+      // 항목 헤더 출력
+      display += "<tr class='header' >";
+      display += "<th width='80' >ID</th>";
+      display += "<th width='320' >상품명</th>";
+      display += "<th width='100' >가격</th>";
+      display += "</tr>";
+
+      // 데이터 목록 출력
+      for (item of list) {
+        display += `<tr class='record' >`;
+        display += `<td align="center" >${item.itemid}</td>`;
+        display += `<td align="left"><a href="#" id="item${item.itemid}">${item.itemname}</td>`;
+        display += `<td align="right" >${item.price} 원</td>`;
+        display += `</tr>`;
+      }
+
+      display += `</table></div>`;
+
+      // 더보기 구현
+      // 현재 페이지가 마지막 페이지가 아닌 경우만 출력
+      if ((pageno - 1) * 5 < count) {
+        display += `<table align="center" width="500" id="tableBtn">`;
+        display += `<tr><td align="center" colspan="3">`;
+        display += `<span id="addBtn"><a>더보기</a></span></td>`;
+        display += `</tr></table>`;
+      }
+
+      content.innerHTML = display;
+
+      // 더보기 버튼을 눌렀을 때 처리
+      let addBtn = document.getElementById("addBtn");
+
+      // addBtn이 존재하는 경우에만 수행
+      if (addBtn != undefined) {
+        addBtn.addEventListener("click", (e) => {
+          pageno += 1;
+          let request = new XMLHttpRequest();
+          request.open("GET", `/item/list?pageno=${pageno}`);
+          request.send("");
+
+          // 전체 데이터 개수보다 더 많이 출력하면 더보기 영역을 삭제
+          if (pageno * 5 >= data.count) {
+            pageno -= 1;
+            document.getElementById("tableBtn").remove();
+          }
+
+          // 데이터를 가져오면
+          request.addEventListener("load", () => {
+            let data = JSON.parse(request.responseText);
+            let list = data.list;
+
+            // 데이터 테이블 출력
+            const table = document.getElementById("tableData");
+
+            let display = "";
+            for (item of list) {
+              display += `<tr class="record">`;
+              display += `<td align="center">${item.itemid}</td>`;
+              // 상세보기를 위해 제목에 item+실제itemid를 추가해서 id 생성
+              display += `<td align="left"><a href="#" id="item${item.itemid}">${item.itemname}</td>`;
+              display += `<td align="right">${item.price} 원</td>`;
+              display += `</tr>`;
+            }
+
+            table.innerHTML += display;
+          });
+        });
+      }
+    } else {
+      content.innerHTML = "<p>데이터 로드 실패</p>";
+    }
+  });
+});
+
+// itemname을 눌렀을 때 수행할 코드
+// 링크 하나 하나 이벤트 처리를 하는 것은 자원 낭비
+// 부모에 이벤트 처리 코드를 작성
+content.addEventListener("click", (e) => {
+  // 클릭한 대상의 id가 item으로 시작하는 경우만 동작
+  if (e.target.id.startsWith("item")) {
+    // 클릭한 대상의 아이디에서 item을 제외한 부분 (itemid)
+    let itemId = e.target.id.substring(4).trim();
+    // console.log(itemid);
+    // alert(itemid);
+
+    let request = new XMLHttpRequest();
+    request.open("GET", `/item/detail/${itemId}`);
+    request.send("");
+
+    request.addEventListener("load", () => {
+      // console.log(request.responseText);
+
+      let data = JSON.parse(request.responseText);
+
+      if (data.result == true) {
+        // 데이터 가져오기
+        let item = data.item;
+
+        // 출력 내용 생성
+        let display = `<div align="center" class="body">`;
+
+        display += `<h2>상세보기</h2>`;
+
+        display += `<table>`;
+
+        display += `<tr><td><a href="/img/${item.pictureurl}"><img src="/img/${item.pictureurl}" /></a><td>`;
+
+        display += `<td align="center"><table>`;
+
+        display += `<tr height="50"><td width="80">상품명</td>`;
+        display += `<td width="160">${item.itemname}</td>`;
+
+        display += `<tr height="50"><td width="80">가격</td>`;
+        display += `<td width="160">${item.price} 원</td>`;
+
+        display += `<tr height="50"><td width="80">비고</td>`;
+        display += `<td width="160">${item.description}</td></tr>`;
+
+        // 삭제를 위한 DOM 추가
+        display += `<tr><td colspan="2" align="center" width="240">`;
+        display += `<a href="#" id="deleteBtn">데이터 삭제</a>`;
+        display += `</td></tr>`;
+
+        // 수정을 위한 DOM 추가
+        display += `<tr><td colspan="2" align="center" width="240">`;
+        display += `<a href="#" id="updateBtn">데이터 수정</a>`;
+        display += `</td></tr>`;
+
+        display += `</table></td></tr></table>`;
+
+        content.innerHTML = display;
+
+        // 데이터 삭제를 눌렀을 때 처리
+        let deleteBtn = document.getElementById("deleteBtn");
+
+        if (deleteBtn != undefined) {
+          deleteBtn.addEventListener("click", (e) => {
+            // 폼이 없는 경우의 POST 방식 파라미터 만들기
+            let params = `itemid=${item.itemid}`;
+
+            let request = new XMLHttpRequest();
+            request.open("POST", "/item/delete");
+
+            // 폼이 아닌 경우는 form 형식으로 인코딩
+            request.setRequestHeader(
+              "Content-type",
+              "application/x-www-form-urlencoded"
+            );
+
+            request.send(params);
+            request.addEventListener("load", () => {
+              let data = JSON.parse(request.responseText);
+
+              if (data.result) {
+                document.getElementById("listBtn").click();
+                alert("삭제 성공");
+              } else {
+                alert("삭제 실패");
+              }
+            });
+          });
+        }
+
+        // 데이터 수정을 눌렀을 때 처리
+        let updateBtn = document.getElementById("updateBtn");
+
+        if (updateBtn != undefined) {
+          updateBtn.addEventListener("click", (e) => {
+            let request = new XMLHttpRequest();
+
+            request.open("GET", "/item/update");
+            request.send("");
+            request.addEventListener("load", () => {
+              let html = request.responseText;
+
+              content.innerHTML = html;
+            });
+          });
+        } else {
+        }
+      }
+    });
+  }
+});
+
+let insertBtn = document.getElementById("insertBtn");
+insertBtn.addEventListener("click", (e) => {
+  // 삽입 화면 출력
+  content.innerHTML = "";
+
+  let html = `
+      <div>
+        <p></p>
+        <form id="insertForm" enctype="multipart/form-data" method="post">
+          아이템이름
+          <input type="test" name="itemname" id="itemname" />
+          <br/>
+          가격
+          <input type="text" name="price" id="price" />
+          <br/>
+          설명
+          <input type="text" name="description" id="description" />
+          <br/>
+          이미지
+          <input type="file" name="pictureurl" id="pictureurl" />
+          <input type="submit" value="삽입" />
+        </form>
+      </div>
+  `;
+
+  content.innerHTML = html;
+
+  // 폼 안에서 삽입 버튼을 눌렀을 때 처리
+  let insertForm = document.getElementById("insertForm");
+
+  if (insertForm != undefined) {
+    insertForm.addEventListener("submit", (e) => {
+      // 기본 이벤트 제거
+      e.preventDefault();
+
+      // 폼 데이터 찾아오기
+      const formData = new FormData(insertForm);
+      console.log(formData);
+
+      // 폼 데이터 전송
+      let request = new XMLHttpRequest();
+
+      request.open("POST", "/item/insert", true);
+      request.send(formData);
+      request.addEventListener("load", () => {
+        let data = JSON.parse(request.responseText);
+
+        if (data.result) {
+          // 데이터 다시 불러오기
+          listBtn.click();
+        } else {
+          alert("삽입 실패");
+        }
+      });
+    });
+  }
+});
