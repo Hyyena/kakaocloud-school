431535c159b4d15b4ccd83944962c49a4a8d7a9f
Hyyena <sjtxm0320@gmail.com>
2023-01-16 15:02:48 +0900

    :sparkles: feat: initialize the movie review project

---
+++ Spring Boot/MovieReviewProject/src/main/java/gmail/sjtxm0320/moviereviewproject/MovieReviewProjectApplication.java
@@ -0,0 +1,15 @@
+package gmail.sjtxm0320.moviereviewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class MovieReviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewProjectApplication.class, args);
+	}
+
+}
